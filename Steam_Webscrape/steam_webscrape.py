80f99feffb03171da32b5afd0a98f4a03a540fd4
kaiye10 <61768323+kaiye10@users.noreply.github.com>
2021-02-01 20:33:38 -0600

    Create steam_webscrape.py

---
+++ Steam_Webscrape/steam_webscrape.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://store.steampowered.com/specials#p=0&tab=TopSellers'
+page = requests.get(url)
+
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id="TopSellersRows")
+
+games = results.find_all('a')
+
+for game in games:
+    title = game.find(class_= "tab_item_name").text
+    og_price = game.find(class_="discount_original_price").text
+    new_price = game.find(class_="discount_final_price").text
+    link = game.find('href')
+    print(title)
+    print(og_price)
+    print(new_price)
+    print(f"Steam Link: {link}\n")
+    print()
